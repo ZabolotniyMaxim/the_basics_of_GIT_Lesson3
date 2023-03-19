5f1235adf776b785d2e175e4fd80fcf2867048ff
Maksim <zabolotniy.m91@gmail.com>
2023-03-19 18:40:56 +0200

    Add project files.

---
+++ the_basics_of_GIT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace the_basics_of_GIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
